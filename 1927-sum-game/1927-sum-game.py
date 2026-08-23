271f23134c6151a9bdd8a12df73f87b0c3740e93
sahil desai <79762140+johnsnow-web@users.noreply.github.com>
2026-08-23 22:53:11 +0530

    Time: 67 ms (30%), Space: 19.8 MB (65.29%) - LeetHub

--- 1927-sum-game/1927-sum-game.py
+++ 1927-sum-game/1927-sum-game.py
@@ -1,29 +1,14 @@
 class Solution:
     def sumGame(self, num: str) -> bool:
-        n = len(num)
-        half = n // 2
+        half = len(num) // 2
 
         diff = 0
-        q_left = 0
-        q_right = 0
+        q_diff = 0
 
         for i, ch in enumerate(num):
             if ch == '?':
-                if i < half:
-                    q_left += 1
-                else:
-                    q_right += 1
+                q_diff += 1 if i < half else -1
             else:
-                if i < half:
-                    diff += int(ch)
-                else:
-                    diff -= int(ch)
+                diff += int(ch) if i < half else -int(ch)
 
-        # Bob can win only if the fixed difference can exactly
-        # be compensated by the '?' characters.
-        #
-        # Required:
-        # diff = 9 * (q_right - q_left) / 2
-        #
-        # Avoid floating point by multiplying both sides by 2.
-        return 2 * diff != 9 * (q_right - q_left)
+        return 2 * diff + 9 * q_diff != 0
